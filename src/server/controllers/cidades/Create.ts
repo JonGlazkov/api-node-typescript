a1c71893747d10a5031268dd7e8e3bc98ca9bd47
Jon Glazkov <jonathan_vmacedo@hotmail.com>
2023-01-04 00:03:56 -0300

    feat: Added validation error for all messages at same time and translation for the errors messages

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -2,32 +2,40 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 
-
 interface ICidade {
   nome: string;
-  
+  estado: string;
 }
 
 const bodyValidation: yup.SchemaOf<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 });
-
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    });
+  } catch (err) {
+    //Catch Error
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
 
-    return res.json({
-      errors: {
-        default: yupError.message,
-      }
+    yupError.inner.forEach((error) => {
+      //For Each error
+      if (!error.path) return;
+
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors,
     });
   }
-  
+
   console.log(req.body.nome);
 
   return res.send("Create!");
